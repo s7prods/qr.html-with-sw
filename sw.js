eef36b453bed3399003ede73e6969eabe48e5dd6
shc0743 <shc0743@outlook.com>
2026-02-06 05:21:18 +0800

    feat(pwa): enhance offline capabilities with improved caching, resources and security

--- sw.js
+++ sw.js
@@ -28,18 +28,27 @@ self.addEventListener("fetch", event => {
 
   const url = new URL(event.request.url);
 
-  if (url.origin !== location.origin) return;
+  if (url.origin !== location.origin || event.request.method !== 'GET') return;
 
-  event.respondWith(
-    caches.match(event.request).then(cached => {
-      if (cached) return cached;
-
-      return fetch(event.request).then(response => {
-        const copy = response.clone();
-        caches.open(CACHE_NAME)
-          .then(cache => cache.put(event.request, copy));
-        return response;
-      });
-    })
-  );
+  event.respondWith((async () => {
+    const cached = await caches.match(event.request);
+    if (cached) {
+      const date = cached.headers.get('date');
+      if (date && navigator.onLine) try {
+        const resp = await fetch(event.request.url, {
+          headers: {
+            'if-modified-since': date
+          }
+        });
+        if (resp.status === 304) return cached;
+        await caches.open(CACHE_NAME).then(cache => cache.put(event.request, resp.clone()));
+        return resp;
+      } catch {}
+      return cached // fallback when offline
+    }
+    
+    const resp = await fetch(event.request);
+    await caches.open(CACHE_NAME).then(cache => cache.put(event.request, resp.clone()));
+    return resp;
+  })());
 });
